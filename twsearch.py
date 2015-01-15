4604615ad2963e9653d046d2fcfa6dc98809b242
peroni <eva.g.596@gmail.com>
2015-01-15 15:54:03 +0100

    New search streaming API functionality

--- twsearch.py
+++ twsearch.py
@@ -26,9 +26,10 @@ except :
 
 TOT_HOURS = 1 # 1 day
 MIN = 12
+KW = ["#JeSuisCharlie", "#CharieHebdo"]
 
 client = MongoClient()
-db = client.twscript
+db = client.twscriptsearch
 lusers = []
 
 
@@ -38,45 +39,36 @@ def createConnection():
 				  OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 	return twitter
 
-def saveUsers(data):
+def processUsers(data):
 	user = data ['user']['screen_name']
 	followers = data ['user']['followers_count']
 	friends = data ['user']['friends_count']
 	ntweets = data ['user']['statuses_count']
 	hashtag = data ['entities']['hashtags']
-	newdata = {"screen_name" : user, "hashtag" : hashtag, "statuses_count" : ntweets, "followers_count" : followers , "friends_count" : friends}
-	db.users.save(newdata)
-	lusers.append(user)
-	print (user)	
+	lang = data ['lang']
+	mentions = data ['entities']['user_mentions']
+	RT_count = data ['retweet_count']
+	reply_user = data['in_reply_to_screen_name']
+	# Text contains retweet (RT) ?
+	text = data ['text']
+	if "RT " in text:
+		RT_bool = True
+	else:
+		RT_bool = False
 
-#For the document with screen_name:user, set the new values. 
-#If the field does not exist, $set will add the field with the specified value. 
-#If you specify a dotted path for a non-existent field
-def updateUsers(upn):
-	
-	laux = lusers
-	updt_users = []
-	while(len(laux) > 1):
-		if(len(laux) >100):
-			updt_users = twitter.lookup_user(screen_name = laux[0:99])
-			laux = laux[99:]
-		else:
-			updt_users = twitter.lookup_user(screen_name = laux[0:len(laux)])
-			laux = {}
+	if ( (RT_count != 0) or (mentions != []) or (reply_user != None) or RT_bool):
 
-		# Update the user in the DB. updt users is a list with users.
-		# each element in the list is a user object. 
-		# updt_users['']
-		print (updt_users)
-		for user in updt_users:
-			newdata = ({ '$set': {
-	       		"followers_count" + upn: user['followers_count'] ,
-	        	"friends_count" + upn: user['friends_count'] ,
-	        	"statuses_count" + upn: user['statuses_count']}})
-			db.users.update({"screen_name": user['screen_name'] }, newdata)
+		newdata =  {"screen_name" : user, "hashtag" : hashtag, "statuses_count" : ntweets, 
+					"followers_count" : followers , "friends_count" : friends, "lang" : lang,
+					"mentions" : mentions, "reply_user" : reply_user, "RT_count": RT_count}
+		db.users.save(newdata)
+		lusers.append(user)
+		print (user)
+	else:
+
+		print ("Not saving user")	
 
 
-	return updt_users
 
 
 class MyStreamer(TwythonStreamer):
@@ -87,7 +79,7 @@ class MyStreamer(TwythonStreamer):
 				#print data ['user']['screen_name']
 				print data['text'].encode('utf-8')
 				db.tweets.save(data)
-				saveUsers(data)
+				processUsers(data)
 				self.counter = self.counter+1
 		else:
 			self.disconnect()
@@ -107,15 +99,10 @@ stream = MyStreamer(APP_KEY, APP_SECRET,
 # IMPORTANT: This loop runs every hour (with the sleep in 12 min and 5 updates.)
 # We need to set the total number of hours: e.g.:24  , 1 day. 
 for hours in range(TOT_HOURS):
-	# 3k users per stream 
-	stream.statuses.sample()
-	for i in range(5):
-		print ("sleeping for 12 minutes, until: " + str(datetime.datetime.now() + datetime.timedelta(minutes= 12) ))
-		time.sleep(MIN * 60)
-		updateUsers(str(i+2))
-
-
-
+	# 3k users per stream
+	# stream.statuses.sample()
+	stream.statuses.filter(track=KW)
+	
 
 
 
